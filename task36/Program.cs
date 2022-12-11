e9bc6f2fe64da9bf4efbfc813e49d666891cefa7
ksanchic <ksanchic@mail.ru>
2022-12-11 15:17:53 +0300

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i+=2)
+    sum = sum + numbers[i];
+
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов cтоящих на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int j = 0; j < numbers.Length; j++)
+        {
+            numbers[j] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int a = 0; a < numbers.Length; a++)
+        {
+            Console.Write(numbers[a] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
